8aa27f83cb6ad456dfa00948aeefcc1383e39cdd
Monika Mona <mona.mt.89@gmail.com>
2022-09-10 21:24:27 +0200

    initial commit

---
+++ src/components/MainSection.js
@@ -0,0 +1,15 @@
+import React from 'react'
+//import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Navigation from './Navigation'
+
+//const Home = () => <Home></Home>
+
+function MainSection() {
+  return (
+    <div className="main-section">
+      <Navigation></Navigation>
+    </div>
+  )
+}
+
+export default MainSection
---
+++ src/components/Navigation.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Navigation() {
+  return (
+    <div>
+      <div>About </div>
+      <div>Shop</div>
+      <div>Gallery</div>
+      <div>Contact</div>
+    </div>
+  )
+}
+
+export default Navigation
