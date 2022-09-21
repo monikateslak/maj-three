6a38abd44cbe79433e2a5b57758b3cc62a2cd6dc
Monika Mona <mona.mt.89@gmail.com>
2022-09-21 12:41:15 +0200

    add MainBox and Footer

---
+++ src/components/MainBox.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function MainBox() {
+  return (
+    <section>
+      <div>boks </div>
+      <div>boks </div>
+      <div>boks </div>
+      <div>boks </div>
+    </section>
+  )
+}
+
+export default MainBox
