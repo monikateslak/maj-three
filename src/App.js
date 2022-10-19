cc0092450f503ad5ad9ac99b58df8920c21e302a
Monika Mona <mona.mt.89@gmail.com>
2022-10-19 20:35:36 +0200

    first-step

--- src/App.js
+++ src/App.js
@@ -1,27 +1,39 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import MainSection from './components/MainSection'
 import logo from './logo.svg'
 import './App.css'
 
+const Home = () => <Home></Home>
+const Navigation = () => <Navigation></Navigation>
+const Footer = () => <Footer></Footer>
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <MainSection></MainSection>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <MainSection></MainSection>
+          <Routes>
+            <Route path={'/'} component={Home} />
+            <Route path={'/nav'} component={Navigation}></Route>
+            <Route path={'/footer'} component={Footer}></Route>
+          </Routes>
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </Router>
   )
 }
 
